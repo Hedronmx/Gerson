14f3c8942050b1b2438e2fed528c3fefaff07259
Thomas Delgado <Thomas Delgado>
2017-04-07 21:40:22 -0700

    removed public folder

--- public/js/javascript/test.js
+++
@@ -1,6 +0,0 @@
-function jstest(){
-  var text = document.getElementById('textarea').value;
-  var textEncrypted = CryptoJS.AES.encrypt(text, "passPhrase");
-  var textDecrypted = CryptoJS.AES.decrypt(textEncrypted, "passPhrase");
-  originalData = textDecrypted.toString(CryptoJS.enc.Utf8);
-}
